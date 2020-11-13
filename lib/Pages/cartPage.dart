51b5acf215366def15eb0ca3bb9fcdbf7cb88710
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-11-13 17:40:48 +0530

    slide to remove item

--- lib/Pages/cartPage.dart
+++ lib/Pages/cartPage.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../AppState.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   AppState appState;
 
   CartPage({
@@ -11,38 +11,62 @@ class CartPage extends StatelessWidget {
   });
 
   @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
   Widget build(BuildContext context) {
     return Container(
         color: Colors.white70,
         child: ListView.separated(
-          itemCount: appState.order.purchasedVegetables.length,
-          separatorBuilder: (context, index) =>
-              Divider(
-                color: Colors.black12,
-                height: 5,
-                thickness: 1.2,
-                indent: 75,
-                endIndent: 75,
-              ),
-          itemBuilder: (context, index) =>
-              Container(
+          itemCount: widget.appState.order.purchasedVegetables.length,
+          separatorBuilder: (context, index) => Divider(
+            color: Colors.black12,
+            height: 5,
+            thickness: 1.2,
+            indent: 75,
+            endIndent: 75,
+          ),
+          itemBuilder: (context, index) => Dismissible(
+              key: Key('${widget.appState.order.purchasedVegetables[index].name}'),
+              onDismissed: (direction) {
+                setState(() {
+                  widget.appState.order.purchasedVegetables.removeAt(index);
+                });
+              },
+              child: Container(
                   height: 100,
                   color: Colors.white,
                   child: Row(
                     children: <Widget>[
+                      SizedBox(width: 20),
                       Image.asset(
-                          appState.order.purchasedVegetables[index].imageUrl,
-                          height: 100, width: 100, fit: BoxFit.contain),
-                      Text('${appState.order.purchasedVegetables[index].name}',
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 24.0,
-                              decoration: TextDecoration.none),
-                          maxLines: 1,
-                          textDirection: TextDirection.ltr),
+                          widget.appState.order.purchasedVegetables[index].imageUrl,
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.contain),
+                      SizedBox(width: 20),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                              '${widget.appState.order.purchasedVegetables[index].name}',
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 24.0,
+                                  decoration: TextDecoration.none)),
+                          SizedBox(height: 5),
+                          Text(
+                              '${widget.appState.order.purchasedVegetables[index].quantity} gm',
+                              style: TextStyle(
+                                  color: Colors.black38,
+                                  fontSize: 18.0,
+                                  decoration: TextDecoration.none)),
+                        ],
+                      )
                     ],
-                  )),
-        )
-    );
+                  ))),
+        ));
   }
 }
